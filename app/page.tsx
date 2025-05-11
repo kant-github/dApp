3c37130aca7371049b5446e5683843fa736e98c6
Rishi Kant <kantrishi7779@gmail.com>
2025-05-12 03:38:27 +0530

    initial commits

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,47 @@
-import Image from "next/image";
+'use client';
+
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { WalletDisconnectButton, WalletMultiButton, } from '@solana/wallet-adapter-react-ui';
+// import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [solAmount, setSolAmount] = useState<number>(0);
+  const wallet = useWallet();
+  const { connection } = useConnection();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  async function handleSendAirdrop() {
+    if (!wallet.publicKey) {
+      alert("Wallet is misisng");
+      return;
+    }
+
+    if (solAmount < 0) {
+      alert("Please add valid SOL amount");
+      return;
+    }
+
+    await connection.requestAirdrop(wallet.publicKey, solAmount);
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-950 px-4">
+      <section className="flex flex-col items-center gap-4">
+
+        <div className="flex gap-2">
+          <WalletMultiButton />
+          <WalletDisconnectButton />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {wallet.publicKey && (
+          <p className="text-neutral-100 text-sm">
+            Hi <span className="font-mono">{wallet.publicKey.toString()}</span>
+          </p>
+        )}
+
+        <input onChange={(e) => setSolAmount(Number(e.target.value))} type="text" placeholder="Enter amount in SOL" className="w-64 px-3 py-2 rounded-md border border-neutral-700 bg-transparent text-neutral-100 placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <button className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition" onClick={handleSendAirdrop} type='button'>Request Airdrop</button>
+
+      </section>
+    </main>
   );
 }
